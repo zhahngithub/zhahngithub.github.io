92071d793d1d5c85fb7a04a57dd3d35ed290b4e8
zhahngithub <63442511+zhahngithub@users.noreply.github.com>
2022-02-24 07:05:21 +0700

    Add files via upload

---
+++ incremental/js/main.js
@@ -0,0 +1,28 @@
+var gameData = {
+  Coins: 0,
+  CoinsPerClick: 1,
+  Factory1Cost: 10
+}
+
+function generateCoins()
+{
+	gameData.Coins += gameData.CoinsPerClick
+	document.getElementById("GeneratedCoins").innerHTML = gameData.Coins + " Coins"
+}
+
+function buyFactory1()
+{
+  if (gameData.Coins >= gameData.Factory1Cost) 
+  {
+    gameData.Coins -= gameData.Factory1Cost
+    gameData.CoinsPerClick += 1
+    gameData.Factory1Cost *= 2
+	document.getElementById("GeneratedCoins").innerHTML = gameData.Coins + " Coins"
+    document.getElementById("perFactory1Upgrade").innerHTML = "Upgrade Factory (Currently Level " + gameData.CoinsPerClick + ") Cost: " + gameData.Factory1Cost + " Coins"
+  }
+}
+
+var mainGameLoop = window.setInterval(function() 
+{
+  generateCoins()
+}, 1000)
