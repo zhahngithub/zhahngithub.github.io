bda49d5052006e1a646250681d3293c346cf2c6c
zhahngithub <63442511+zhahngithub@users.noreply.github.com>
2022-02-24 13:37:14 +0700

    1.1 ??
    i hope this works

--- incremental/README.md
+++ incremental/README.md
@@ -1,2 +1,7 @@
-# Helo
-idk what to write here
+# idk what to write so ill just yeet the latest changelog
+
+1.1 - 24 2 2022
+Added Save and Load
+Added Reset :flushed:
+Added Auto Load when the website started
+thanks to [Tanktot Games]
--- incremental/js/main.js
+++ incremental/js/main.js
@@ -1,13 +1,16 @@
+//Main Data
 var gameData = {
   Coins: 0,
   CoinsPerClick: 1,
   Factory1Cost: 10
 }
 
+//Main Game Thing
 function generateCoins()
 {
 	gameData.Coins += gameData.CoinsPerClick
 	document.getElementById("GeneratedCoins").innerHTML = gameData.Coins + " Coins"
+  document.getElementById("perFactory1Upgrade").innerHTML = "Upgrade Factory (Currently Level " + gameData.CoinsPerClick + ") Cost: " + gameData.Factory1Cost + " Coins"
 }
 
 function buyFactory1()
@@ -17,16 +20,36 @@ function buyFactory1()
     gameData.Coins -= gameData.Factory1Cost
     gameData.CoinsPerClick += 1
     gameData.Factory1Cost *= 2
-	document.getElementById("GeneratedCoins").innerHTML = gameData.Coins + " Coins"
+	  document.getElementById("GeneratedCoins").innerHTML = gameData.Coins + " Coins"
     document.getElementById("perFactory1Upgrade").innerHTML = "Upgrade Factory (Currently Level " + gameData.CoinsPerClick + ") Cost: " + gameData.Factory1Cost + " Coins"
   }
 }
 
+//Extra Feature
 function saveGame() {
 	var gameSave = {
 		gameData
 	};
 	localStorage.setItem("gameSave", JSON.stringify(gameSave));
+}
+
+function loadGame() {
+  var savedGame = JSON.parse(localStorage.getItem("gameSave"));
+  if (typeof savedGame.gameData !== "undefined") gameData = savedGame.gameData;
+  generateCoins()
+}
+
+function resetGame() {
+  if (confirm("why would u reset but welp here confirmation ig")) {
+    var gameSave = {};
+    localStorage.setItem("gameSave", JSON.stringify(gameSave));
+    location.reload();
+  }
+}
+
+//Auto Stuff
+window.onload = function() {
+  loadGame();
 }
 
 var mainGameLoop = window.setInterval(function() 
